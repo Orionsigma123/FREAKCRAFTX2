d7ce5b6dd501596f56dbf8251fe602ba02ca8875
Orionsigma123 <sigmaorion77@gmail.com>
2024-10-20 17:33:30 -0400

    Update player.js

--- player.js
+++ player.js
@@ -2,11 +2,13 @@ class Player {
     constructor(camera) {
         this.camera = camera;
         this.position = new THREE.Vector3(0, 10, 0); // Starting position
-        this.speed = 0.1;
+        this.speed = 0.1; // Movement speed
     }
 
     update(keyboardState) {
         const direction = new THREE.Vector3();
+
+        // Check keyboard state for movement
         if (keyboardState['KeyW']) direction.z -= 1; // Move forward
         if (keyboardState['KeyS']) direction.z += 1; // Move backward
         if (keyboardState['KeyA']) direction.x -= 1; // Move left
@@ -14,6 +16,12 @@ class Player {
 
         direction.normalize().multiplyScalar(this.speed);
         this.position.add(direction);
+
+        // Update camera position and direction
         this.camera.position.set(this.position.x, this.position.y, this.position.z);
         this.camera.lookAt(this.position.x, this.position.y, this.position.z - 1); // Look direction
     }
+}
+
+// Ensure the Player class is defined
+console.log('Player class defined:', typeof Player !== 'undefined');
